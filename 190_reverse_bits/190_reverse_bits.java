b4b8f867b0e22bee753e45d73c5b2092c95823d3
Emma Mascillaro <emascillaro@gmail.com>
2026-02-15 13:11:18 -0500

    complete leetcode 190

---
+++ 190_reverse_bits/190_reverse_bits.java
@@ -0,0 +1,54 @@
+/*
+Reverse bits of a given 32 bits signed integer.
+
+ 
+
+Example 1:
+
+Input: n = 43261596
+
+Output: 964176192
+
+Explanation:
+Integer	Binary
+43261596	00000010100101000001111010011100
+964176192	00111001011110000010100101000000
+
+Example 2:
+
+Input: n = 2147483644
+
+Output: 1073741822
+
+Explanation:
+Integer	Binary
+2147483644	01111111111111111111111111111100
+1073741822	00111111111111111111111111111110
+
+ 
+
+Constraints:
+
+    0 <= n <= 231 - 2
+    n is even.
+
+ 
+
+Follow up: If this function is called many times, how would you optimize it?
+*/
+
+class Solution {
+  public int reverseBits(int n) {
+    int result = 0;
+
+    for (int i = 0; i < 32; i++) {
+      // extract least significant bit
+      int least_significant_bit = n & 1;
+      // append but to result
+      result = (result << 1) | least_significant_bit;
+      // right shift n to process next bit
+      n = n >> 1;
+    }
+    return result;
+  }
+}
